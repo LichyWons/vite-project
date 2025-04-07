da0b13b54141bf7fcee5ef1cc9bfa1c58a2b4761
LichyWons <124511088+LichyWons@users.noreply.github.com>
2025-04-07 22:20:18 +0200

    create repo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+	content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
